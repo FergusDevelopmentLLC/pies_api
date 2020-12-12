7045894104a60480fa57becb0ad6b9eaa32d79ac
Will Carter <will.carter@fergusllc.com>
2020-12-11 17:13:14 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   end
 
   resource :user, only: [:show, :update]
-  resources :users
   
   resources :pies
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
