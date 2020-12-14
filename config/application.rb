3de4a7ddfd6dcbf59307434a4f41387b3fabfc95
Will Carter <will.carter@fergusllc.com>
2020-12-14 12:52:32 -0700

    allow delete

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module PiesApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+         resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
        end
     end
   end
